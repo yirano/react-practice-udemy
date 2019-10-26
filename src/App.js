f4ce8fe29633e0a69cd542f42c5d60287c8c1fd1
yirano <yirano@protonmail.ch>
2019-10-25 23:11:08 -0400

    End of lecture project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
   }
 
@@ -16,11 +17,25 @@ class App extends React.Component {
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
   }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
   render() {
-    return (<div className='App'>
-      <SearchBox />
-      <CardList monsters={this.state.monsters} />
-    </div>
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return (
+      <div className='App'>
+        <h1> Monsters Rolodex </h1>  
+        <SearchBox 
+          placeholder = 'Search monsters'
+          handleChange = {e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
     )
   }
 }
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './search-box.styles.css';
 
-export const SearchBox = () => (
-  <div className="">
-    <input type="search" name="search" id="search" placeholder="Search" />
-  </div>
-);
+export const SearchBox = ({ placeholder, handleChange }) => (
+  <input 
+    className = 'search'
+    type='search' 
+    placeholder={placeholder}
+    onChange={handleChange}
+  />
+)
