e0c45110ac68cd7463232b65ebc28bbc43a016a6
yirano <yirano@protonmail.ch>
2019-10-25 23:38:59 -0400

    Updates

---
+++ precache-manifest.4bf5b297216f240c86d4a5180963a80a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6380ab70b4be13e51036882037f244fd",
+    "url": "/react-practice-udemy/index.html"
+  },
+  {
+    "revision": "8f9f73d3ad36481dfe0b",
+    "url": "/react-practice-udemy/static/css/main.335d12ce.chunk.css"
+  },
+  {
+    "revision": "f3a9916f0e827d8beb89",
+    "url": "/react-practice-udemy/static/js/2.67a09714.chunk.js"
+  },
+  {
+    "revision": "8f9f73d3ad36481dfe0b",
+    "url": "/react-practice-udemy/static/js/main.768e9618.chunk.js"
+  },
+  {
+    "revision": "eb91c8cc2f605d33dc74",
+    "url": "/react-practice-udemy/static/js/runtime-main.0433120e.js"
+  }
+]);
